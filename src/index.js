7e606feda2cb4b53f8c21a8eb861530f9b7fa0cd
Guillermo Varelli <guillermovarelli@Guillermos-MacBook-Air.local>
2025-02-25 22:03:38 -0300

    change indec

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import ApexChart from "./ApexChart"; // Versión para escritorio
 import ApexChartMobile from "./ApexChartMobile"; // Versión para móvil
 import reportWebVitals from "./reportWebVitals";
 
+const isMobileDevice = () => {
+  return /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent) || window.innerWidth < 768;
+};
+
 const ResponsiveChart = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [isMobile, setIsMobile] = useState(isMobileDevice());
 
   useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth < 768);
+    const handleResize = () => setIsMobile(isMobileDevice());
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
