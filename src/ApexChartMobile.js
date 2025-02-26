479cab4ee1de97032d353311e6e9635eb6be58d9
Guillermo Varelli <guillermovarelli@Guillermos-MacBook-Air.local>
2025-02-25 22:12:43 -0300

    change indec

--- src/ApexChartMobile.js
+++ src/ApexChartMobile.js
@@ -40,7 +40,7 @@ const ApexChartMobile = () => {
           series,
           options: {
             chart: {
-              height: 500,
+              height: 600, // Aumento la altura del gráfico
               width: 500,
               type: "heatmap",
             },
@@ -73,7 +73,8 @@ const ApexChartMobile = () => {
               title: { text: "Dates" },
               labels: {
                 style: {
-                  fontSize: "10px",
+                  fontSize: "14px", // Aumento el tamaño de la fuente de las fechas
+                  fontWeight: 600,  // Hago el texto un poco más grueso
                 },
               },
             },
@@ -89,7 +90,7 @@ const ApexChartMobile = () => {
   return (
     <div>
       <h2>Activity Heatmap</h2>
-      <ReactApexChart options={chartData.options} series={chartData.series} type="heatmap" height={500} width={500} />
+      <ReactApexChart options={chartData.options} series={chartData.series} type="heatmap" height={600} width={500} />
     </div>
   );
 };
